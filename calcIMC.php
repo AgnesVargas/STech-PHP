d728ebf2d3465f5d12c4b93e6c5ae7919451520b
agnesvargas <agnes.vitoria@warrem.com.br>
2022-07-12 14:59:53 -0300

    up

---
+++ calcIMC.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>Aula 08</title>
+
+</head>
+
+<body>
+    <?php
+    echo "O seu nome é: " . $_POST["nome"];
+    echo "</br>";
+    echo "O seu peso é: " . $_POST["peso"];
+    echo "</br>";
+    echo "O seu altura é: " . $_POST["altura"];
+    echo "</br>";
+
+    $peso = $_POST['peso'];
+    $altura = $_POST['altura'];
+    
+    
+
+    function imc($altura, $peso)
+    {
+        $altura = str_replace(',', '.', $altura);
+        $result = $peso / ($altura * $altura);
+        return $result;
+    }
+    echo "<hr/>";
+    echo "Seu IMC é: " . imc($altura, $peso);
+    echo "</br>";
+    if (imc($altura, $peso)< 18.5){
+        echo "Abaixo do Peso";
+    }elseif (imc($altura, $peso) <= 24.9){
+        echo "Peso Normal";
+    }elseif (imc($altura, $peso) <= 29.9){
+        echo "Sobrepeso";
+    }elseif (imc($altura, $peso) <= 34.9){
+        echo "Obesidade Grau I";
+    }elseif (imc($altura, $peso) <= 39.9){
+        echo "Obesidade Grau II";
+    }else{
+        echo "Obesidade Grau III ou Mórbida";
+    }
+
+    ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 08</title>
+</head>
+
+<body>
+    <?php
+    // put your code here
+    echo "<h1>Trabalhando com variáveis</h1>";
+    $num1 = 1;
+    $num2 = 2;
+    $result = $num1 + $num2;
+    echo ("Op1: O resultado da soma é: " . ($num1 + $num2));
+    echo "</br>";
+    echo ('Op2: O resultado da soma é: <span style="color:blue">' . ($num1 + $num2) . '</span>');
+    echo "</br>";
+    echo ("Op3: O resultado da soma é: <span style = 'color:blue'> $result </span>");
+
+    echo "<hr/>";
+    $nome = "Agnes Vargas";
+    $idade = "18";
+
+    if($idade >= 18){
+        echo "$nome, você é maior de idade.";
+    }
+    else{
+        echo "$nome, você é menor de idade.";
+    }
+
+    echo "<hr/>";
+    $nota1 = 8;
+    $nota2 = 6;
+    $nota3 = 10;
+    $result = ($nota1 + $nota2 + $nota3)/3;
+    echo (" A média calculada foi: <span style = 'color:blue'> $result </span>");
+
+    echo "<hr/>";
+    $funcionario = "juliana";
+    $salario = 10800;
+    $INSS = 11;
+    $INSSDesconto = ($salario / 100 * $INSS);
+    $salarioLiquido = $salario - ($salario/100 * 11) ;
+    $FGTS = 10800 * 0.08;
+    echo ("O salário bruto do funcionário " .  ($funcionario) . ' é: ' . ($salario));
+    echo "</br>";
+    echo ("O salário líquido do funcionário é: " . ($salarioLiquido));
+    echo "</br>";
+    echo ("O INSS do funcionário é: " . ($INSSDesconto));
+    echo "</br>";
+    echo ("O FGTS do funcionário é: " .($FGTS));
+
+    echo "<hr/>";
+    $nome = "Juliana";
+    $altura = 1.57;
+    $peso = 53;
+    ?>
+    <form action = "calcIMC.php" method = "POST">
+        <fieldset>
+        <label for = "nome"> Nome Completo: </label>
+        <input type = "text" name = "nome" id = "nome">
+        </br>
+        <label for = "altura"> Altura: </label>
+        <input type = "text" name = "altura" id = "altura">
+        </br>
+        <label for = "peso"> Peso: </label>
+        <input type = "int" name = "peso" id = "peso">
+        </br>
+        <input type="submit" value="Enviar" id="enviar">
+        <input type = "reset" value = "Limpar" id = "limpar">
+        </fieldset>
+    </form>
+</body>
+
+</html>
